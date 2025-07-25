ff44ae8c1706e65907f1897eb45a01996b2522b8
oricardos <oricardo.dev@gmail.com>
2025-07-25 20:18:20 -0300

    [ESTRUTURA CONDICIONAL]Exercícios

---
+++ Estrutura Condicional/Main.java
@@ -0,0 +1,128 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int hours;
+//        hours = sc.nextInt();
+//
+//        if (hours < 12){
+//            System.out.println("Bom dia");
+//        } else if (hours < 18){
+//            System.out.println("Boa tarde");
+//        } else {
+//            System.out.println("Boa noite");
+//        }
+//
+//        sc.close();
+
+//        Exercícios
+//        Exercício 1 --------------------------------------------------------------------------------------------------
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        if (n < 0) {
+//            System.out.println("NEGATIVO");
+//        } else {
+//            System.out.println("NAO NEGATIVO");
+//        }
+//        Exercício 2 --------------------------------------------------------------------------------------------------
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        if (n % 2 == 0) {
+//            System.out.println("PAR");
+//        } else {
+//            System.out.println("IMPAR");
+//        }
+
+//        Exercício 3 --------------------------------------------------------------------------------------------------
+//        Scanner sc = new Scanner(System.in);
+//        int A = sc.nextInt();
+//        int B = sc.nextInt();
+//
+//        if (A % B == 0 || B % A == 0){
+//            System.out.println("Sao Multiplos");
+//        } else {
+//            System.out.println("Nao sao Multiplos");
+//        }
+
+//        Exercício 4 --------------------------------------------------------------------------------------------------
+//        Scanner sc = new Scanner(System.in);
+//        int i = sc.nextInt();
+//        int f = sc.nextInt();
+//
+//        int duracao;
+//        if (i < f){
+//            duracao = f - i;
+//        } else {
+//            duracao = 24 - i + f;
+//        }
+//
+//        System.out.println("O JOGO DUROU " + duracao + " HORA(S)");
+
+//        Exercício 5 --------------------------------------------------------------------------------------------------
+//        Locale.setDefault(Locale.US);
+//        Scanner sc = new Scanner(System.in);
+//        int code = sc.nextInt();
+//        int qtt = sc.nextInt();
+//
+//        double total;
+//        if (code == 1) {
+//            total = qtt * 4.00;
+//        } else if (code == 2) {
+//            total = qtt * 4.50;
+//        } else if (code == 3) {
+//            total = qtt * 5.00;
+//        } else if (code == 4) {
+//            total = qtt * 2.00;
+//        } else {
+//            total = qtt * 1.50;
+//        }
+//
+//        System.out.printf("Total: R$ %.2f%n", total);
+
+//        Exercício 6 --------------------------------------------------------------------------------------------------
+//        Locale.setDefault(Locale.US);
+//        Scanner sc = new Scanner(System.in);
+//
+//        double value = sc.nextDouble();
+//        if (value < 0 || value > 100.0) {
+//            System.out.println("Fora de intervalo");
+//        } else if (value <= 25 && value > 0) {
+//            System.out.println("Intervalo [0,25]");
+//        } else if (value <= 50) {
+//            System.out.println("Intervalo (25,50]");
+//        } else if (value <= 75) {
+//            System.out.println("Intervalo (50,75]");
+//        } else if (value <= 100) {
+//            System.out.println("Intervalo (75,100]");
+//        }
+//
+//        Exercício 7 --------------------------------------------------------------------------------------------------
+//        Locale.setDefault(Locale.US);
+//        Scanner sc = new Scanner(System.in);
+//        double x = sc.nextDouble();
+//        double y = sc.nextDouble();
+//
+//        if (x == 0.0 && y == 0.0){
+//            System.out.println("Origem");
+//        } else if (x > 0.0 && y > 0.00){
+//            System.out.println("Q1");
+//        } else if (x < 0.0 && y > 0.00){
+//            System.out.println("Q2");
+//        } else if (x < 0.0 && y < 0.00){
+//            System.out.println("Q3");
+//        } else {
+//            System.out.println("Q4");
+//        }
+
+//        Exercício 8 --------------------------------------------------------------------------------------------------
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+//TODO CONTINUAR
+
+        sc.close();
+    }
+}
